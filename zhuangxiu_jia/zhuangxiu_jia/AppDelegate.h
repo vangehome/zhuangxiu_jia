4ee200b52c181d5e0f650b7e3c4bf6488c871ce1
vangehome <vanghome@qq.com>
2016-04-06 22:26:40 +0800

    创建项目

---
+++ zhuangxiu_jia/zhuangxiu_jia/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  zhuangxiu_jia
+//
+//  Created by ANDY on 16/4/6.
+//  Copyright © 2016年 vangehome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
